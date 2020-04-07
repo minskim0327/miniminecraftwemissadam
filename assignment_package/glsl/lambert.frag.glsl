742a7d775ccac80aaeb460e804f4dd5eb59e819d
Changhun-0305 <changhun0305@gmail.com>
2020-04-07 20:15:34 +0900

    conflict resolved

--- assignment_package/glsl/lambert.frag.glsl
+++ assignment_package/glsl/lambert.frag.glsl
@@ -59,11 +59,7 @@ float cubicTriMix(vec3 p) {
 float fbm(vec3 p) {
     float amp = 0.5;
     float freq = 4.0;
-<<<<<<< HEAD
-    float sum = 0.0;
-=======
     float sum = 0.f;
->>>>>>> master
     for(int i = 0; i < 8; i++) {
         sum += cubicTriMix(p * freq) * amp;
         amp *= 0.5;
